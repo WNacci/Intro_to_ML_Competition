67be758f2084806c947f13179f1e8fe0a29aa4b9
William Nacci <wnacci@gmail.com>
2023-11-05 21:14:25 -0600

    Implemented data splitting functions.

--- Data_Splitting.py
+++ Data_Splitting.py
@@ -1,2 +1,44 @@
 # Options for splitting
 # Functions for train/test, train/test/val/etc
+from sklearn.model_selection import train_test_split
+from imblearn.over_sampling import RandomOverSampler
+
+
+
+
+class split_data:
+    def __init__(self, train_x, train_y, test_x, test_y,val_x=None,val_y=None,query_x=None,query_y=None):
+        self.trainx = train_x
+        self.trainy = train_y
+        self.testx = test_x
+        self.testy = test_y
+        self.valx = val_x
+        self.valy = val_y
+        self.queryx = query_x
+        self.queryy = query_y
+        if(train_x is not None and train_y is not None and val_x is not None and val_y is not None and query_x is not None and query_y is not None):
+            self.t = "train_test_val_query"
+        elif(train_x is not None and train_y is not None and val_x is not None and val_y is not None):
+            self.t = "train_test_val"
+        elif(train_x is not None and train_y is not None):
+            self.t = "train_test"
+        else:
+            self.t = "improper format"
+            
+    def resample(self):
+        # oversample connected neuron pairs
+        ros = RandomOverSampler(random_state=0)
+        # X_resampled, y_resampled = ros.fit_resample(train_data[["fw_similarity", "adp_dist"]], train_data["connected"])
+        # Oversample but with all features
+        self.trainx, self.trainy = ros.fit_resample(self.trainx,self.trainy)
+    
+
+def train_test(data, test_size=0.2):
+    train_data, test_data = train_test_split(data, test_size=test_size, random_state=1)
+    train_x = train_data.loc[:, train_data.columns != "connected"]
+    train_y = train_data["connected"]
+    test_x = test_data.loc[:, test_data.columns != "connected"]
+    test_y = test_data["connected"]
+    
+    return (split_data(train_x,train_y,test_x,test_y))
+
